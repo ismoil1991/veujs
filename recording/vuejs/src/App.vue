bdf84fd6ce6c3720b514b350fc8bf852e6a84826
ismile <voyasa9172@latovic.com>
2022-01-18 17:00:34 +0500

    nesting components

--- recording/vuejs/src/App.vue
+++ recording/vuejs/src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p>{{ greeting() }}</p>
+    <ninjas></ninjas>
   </div>
 </template>
 
 <script>
+import Ninjas from './Ninjas.vue'
+
 export default {
+  components:{
+    'ninjas':Ninjas
+  },
   data() {
     return {
-      title:'Your first Vue file'
-    }
-  },
-  methods:{
-    greeting(){
-      return 'hey';
+      title:'Ninja App'
     }
   }
 }
